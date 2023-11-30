6dcd221831e9323313ecd454426c535b14bf069e
Nikola Stankovski <stankovski.n@hotmail.com>
2023-11-30 20:34:24 +0100

    .

--- python scripts/selenium scraping.py
+++ python scripts/selenium scraping.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+
 import time
 import threading
 import csv
@@ -8,7 +9,9 @@ import csv
 
 class Browser:
     def __init__(self) -> None:
-        self.browser = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        self.browser = webdriver.Chrome(options=options)
 
     def go_to(self, url:str):
         self.browser.get(url)
@@ -34,26 +37,69 @@ class Browser:
         except:
             return []
 
+class Arguments:
+    def __init__(self) -> None:
+        self.callbackFn = None
+        self.base_url = None
+        self.start_page = None
+        self.end_page = None
+        self.page_steps = None
+        self.step = None
+        self.no_of_instances = None
 
-def scrape_Skinsort():
-    BASE_ULR_SKINSORT = "https://skinsort.com/ingredients"
+def get_step_pages(no_of_pages, no_of_instances = 5):
+    per_page = int(no_of_pages / no_of_instances)    
     
+    if per_page == 0:
+        per_page = no_of_pages
+
+    pages = [i for i in range(0, no_of_pages, per_page)]
+
+    groups = []
+    for i in range(len(pages)):
+        
+        next_num = None
+        if i+1 >= len(pages):
+            next_num = no_of_pages
+        else:
+            next_num = pages[i + 1]
+        
+        groups.append({"start_page": pages[i] + 1, "end_page": next_num})
+    
+    return groups
+
+def initiate_drivers(args:Arguments):
+    scraper_arguments = []
+    for item in args.page_steps:
+        scraper_args = Arguments()
+        scraper_args.start_page = item["start_page"]
+        scraper_args.end_page = item["end_page"]
+        scraper_args.base_url = args.base_url
+        scraper_arguments.append(scraper_args)
+
+    thread_list = []
+
+    for i in range(len(scraper_arguments)):
+        thread_list.append(threading.Thread(target=args.callbackFn, args=[scraper_arguments[i]]))
+
+    for i in range(len(thread_list)):
+        thread_list[i].start()
+        
+def scrape_skinsort1(args:Arguments):     
     browser = Browser()
-    browser.go_to(BASE_ULR_SKINSORT)
     
-    last_page_item = browser.find_element(".page-item:last-child a")
-    no_of_pages = int(last_page_item.get_attribute("href").split("/")[-1])
+    FILE_PATH = f'./Data/skinsort/ingredients_{args.start_page}_{args.end_page}.csv'
     
-    with open('./Data/ingredient_w_synonyms.csv', 'w', newline='',encoding="utf-8") as file:
+    with open(FILE_PATH, 'w', newline='', encoding="utf-8") as file:
         file.truncate()
         writer = csv.writer(file, delimiter=";")
         field = ["name", "synonym"]
         
         writer.writerow(field)
         
-        for i in range(no_of_pages):
+        for i in range(args.start_page, args.end_page + 1):
             page_no = i+1
-            url = BASE_ULR_SKINSORT + "/page/" + str(page_no)
+            url = args.base_url + "/page/" + str(page_no)
             browser.go_to(url)
                 
             page_elems = browser.find_elements("#ingredients-table div > a")
@@ -77,47 +123,29 @@ def scrape_Skinsort():
                 finally:
                     writer.writerow([ingredient_name, ingredient_synonyms])
                         
-        browser.quit()
-   
+    browser.quit()
 
-def test():
-    def test_logic():
-        driver = webdriver.Chrome()
-        url = 'https://www.google.co.in'
-        driver.get(url)
-        # Implement your test logic
-        time.sleep(2)
-        driver.quit()
+def scrape_Skinsort():
+    BASE_ULR_SKINSORT = "https://skinsort.com/ingredients"
+    
+    browser = Browser()
+    browser.go_to(BASE_ULR_SKINSORT)
+    
+    last_page_item = browser.find_element(".page-item:last-child a")
+    no_of_pages = int(last_page_item.get_attribute("href").split("/")[-1])
+            
+    browser.quit()
 
-    N = 5   # Number of browsers to spawn
-    thread_list = list()
+    t1 = time.time()
+    print("Start: ", t1)
+    args = Arguments()
+    args.callbackFn = scrape_skinsort1
+    args.page_steps = get_step_pages(no_of_pages)
+    args.base_url = BASE_ULR_SKINSORT    
+    
+    initiate_drivers(args)
+    t2 = time.time()
+    print("End: ", t2)
+    print("Time to finish: ", t2-t1)
 
-    # Start test
-    for i in range(N):
-        t = threading.Thread(name='Test {}'.format(i), target=test_logic)
-        t.start()
-        time.sleep(1)
-        print(t.name + ' started!')
-        thread_list.append(t)
-
-    # Wait for all threads to complete
-    for thread in thread_list:
-        thread.join()
-
-    print('Test completed!')
-
-
-def test1():
-    import pubchempy as pcp
-
-    results = pcp.get_compounds('Niacinamide', 'name')
-
-    if results:
-        compound = results[0]
-        synonyms = compound.synonyms
-        print("Synonyms for Niacinamide ", synonyms)
-    else:
-        print("Compound not found in PubChem.")
-        
-test1()
-#scrape_Skinsort()
+scrape_Skinsort()
